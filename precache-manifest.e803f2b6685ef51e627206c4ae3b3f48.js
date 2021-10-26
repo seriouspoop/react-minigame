e68f00a832a925b909a678b01f11adc6e1527cc3
Harshit Singh <harshit.singh817775@gmail.com>
2021-10-26 20:29:17 +0530

    Updates

---
+++ precache-manifest.e803f2b6685ef51e627206c4ae3b3f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5fc80ce7a34a899b8bbdcc2f9f2994",
+    "url": "/react-minigame/index.html"
+  },
+  {
+    "revision": "ad429d97a0ed6057932c",
+    "url": "/react-minigame/static/css/main.df814b37.chunk.css"
+  },
+  {
+    "revision": "93ee606ab8c7aeafe0bd",
+    "url": "/react-minigame/static/js/2.f524853c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-minigame/static/js/2.f524853c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da346801c337fe6aaf0a",
+    "url": "/react-minigame/static/js/3.b633f61c.chunk.js"
+  },
+  {
+    "revision": "ad429d97a0ed6057932c",
+    "url": "/react-minigame/static/js/main.96f02783.chunk.js"
+  },
+  {
+    "revision": "c9a13e63acc60faa06e3",
+    "url": "/react-minigame/static/js/runtime-main.f2bb3fa1.js"
+  }
+]);
